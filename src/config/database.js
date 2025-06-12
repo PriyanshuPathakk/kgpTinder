54f310b0a74ee499755ceaf3b79616e21bb6c351
PriyanshuPathakk <priyanshupathak296@gmail.com>
2025-06-13 02:39:21 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://priyanshu:f4LqoRwhmF0QjPet@learnmongo.ue0ly23.mongodb.net/kgpTinder") 
+    await mongoose.connect("") 
 }
 
 module.exports = connectDB
